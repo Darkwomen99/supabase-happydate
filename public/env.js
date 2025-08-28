80ae3e46199a01b39bc55e62bf4e796474fd344a
Darkwomen99 <Bilohlazova.m@gmail.com>
2025-08-28 18:38:18 +0200

    опис змін

---
+++ public/env.js
@@ -0,0 +1,5 @@
+// завантажується в браузері ДО supabaseClient.js
+window.ENV = {
+  SUPABASE_URL: "https://luxkdftbmxspkjscdafn.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1eGtkZnRibXhzcGtqc2NkYWZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMzgyMzQsImV4cCI6MjA3MTcxNDIzNH0.eRny9_e-UKjZGQjSNxNnzFiMClgiyTTi-H-qucSXVZo",
+};
--- src/api/env.js
+++
@@ -1,8 +0,0 @@
-// /api/env.js
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.status(200).json({
-    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  });
-}
